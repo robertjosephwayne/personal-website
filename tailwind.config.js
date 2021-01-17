05fda04afa9ba6d1515c87ca2b0e0d7b79c21860
Robert <robertjwayne@gmail.com>
2021-01-17 03:07:12 -0800

    Set color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{html,ts}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      black: "#000",
+      white: "#fff",
+
+      green: colors.emerald,
+      gray: colors.warmGray,
+      blue: colors.lightBlue,
+      purple: colors.violet,
+      red: colors.red,
+      yellow: colors.yellow,
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      width: ["group-hover"],
+      opacity: ["group-hover"],
+      textColor: ["group-hover"],
+    },
   },
   plugins: [],
 };
