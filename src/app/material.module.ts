f09211886497277fe1da3994474cf0337bb91c5b
Robert <robertjwayne@gmail.com>
2021-01-16 23:25:37 -0800

    Add MatButton and MatToolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
-
+    MatButtonModule,
+    MatToolbarModule
   ],
   exports: [
-
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
