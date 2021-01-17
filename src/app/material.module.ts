19c608d66dd34d3df8ccbb974e313ad54a112ee2
Robert <robertjwayne@gmail.com>
2021-01-16 23:59:06 -0800

    Add MatTabsModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatTabsModule,
     MatToolbarModule
   ],
   exports: [
     MatButtonModule,
+    MatTabsModule,
     MatToolbarModule
   ]
 })
