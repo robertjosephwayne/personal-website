87b2f2346378c5f1814d13009776e5c4a4b48aab
Robert <robertjwayne@gmail.com>
2021-01-17 03:07:33 -0800

    Add MatIconModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatIconModule,
     MatTabsModule,
     MatToolbarModule
   ],
   exports: [
     MatButtonModule,
+    MatIconModule,
     MatTabsModule,
     MatToolbarModule
   ]
