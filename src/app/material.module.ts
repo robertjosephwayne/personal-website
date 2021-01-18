eb7452e9cd6d107143154b59568624de7ee54bc2
Robert <robertjwayne@gmail.com>
2021-01-18 03:28:18 -0800

    Add MatCardModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,12 +8,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   imports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
   ],
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
