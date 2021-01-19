ec52bbc606fa6e234e1d46e69c7df24b3062cdb4
Robert <robertjwayne@gmail.com>
2021-01-19 00:54:01 -0800

    Add MatDividerModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
@@ -16,6 +18,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
